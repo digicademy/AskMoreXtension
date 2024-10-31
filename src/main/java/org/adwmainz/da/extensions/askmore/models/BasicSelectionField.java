acc9d914ea8dd52ed884b84d1236894e95a2f757
pdbro2k <40125493+pdbro2k@users.noreply.github.com>
2024-10-31 15:56:39 +0100

    slightly improved BasicSelectionField

--- src/main/java/org/adwmainz/da/extensions/askmore/models/BasicSelectionField.java
+++ src/main/java/org/adwmainz/da/extensions/askmore/models/BasicSelectionField.java
@@ -1,5 +1,5 @@
 /**
- * SelectionField.java - is a generic extension of a org.adwmainz.da.extensions.askmore.models.BasicInputField representing a simple selection field as used within
+ * BasicSelectionField.java - is a generic extension of a org.adwmainz.da.extensions.askmore.models.BasicInputField representing a simple selection field as used within
  *  the AskMoreXtension developed at the Digital Academy of the Academy of Sciences and Literature | Mainz.
  * @author Patrick D. Brookshire
  * @version 1.0.0
@@ -12,13 +12,14 @@ import java.util.Set;
 public class BasicSelectionField<T> extends BasicInputField<T> {
 
 	// additional fields
-	Set<SelectableOption<T>> options;
-	boolean isEditable;
+	protected Set<SelectableOption<T>> options;
+	protected boolean isEditable;
 	
 	// constructors
 	/**
 	 * Creates a new SelectionField with the specified options
 	 * @param options a set of options that should be selectable from this input field
+	 * @param isEditable a boolean controlling whether a user should be able to add options or not
 	 */
 	public BasicSelectionField(Set<SelectableOption<T>> options, boolean isEditable) {
 		super();
@@ -29,6 +30,7 @@ public class BasicSelectionField<T> extends BasicInputField<T> {
 	/**
 	 * Creates a new SelectionField with the specified options and a default option
 	 * @param options a set of options that should be selectable from this input field
+	 * @param isEditable a boolean controlling whether a user should be able to add options or not
 	 * @param defaultOption the option that should be selected per default
 	 */
 	public BasicSelectionField(Set<SelectableOption<T>> options, boolean isEditable, SelectableOption<T> defaultOption) {
